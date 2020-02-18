d24289a8639afede1c9dbb1aa3b59de63b365b74
Amy-Johnston <60958102+Amy-Johnston@users.noreply.github.com>
2020-02-18 17:40:58 +0200

    Update App.java

--- testingMaven1/testingMaven1/testingMaven1/src/main/java/za/co/cput/maven/App.java
+++ testingMaven1/testingMaven1/testingMaven1/src/main/java/za/co/cput/maven/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! \n This is my first Maven project");
+        System.out.println( "I did my first branch -branch1Mavenproject- and I edited it" );
     }
 }
