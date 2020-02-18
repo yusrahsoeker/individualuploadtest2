09090457b60e6de2c9a5a4595af6ad916b741f7b
Amy-Johnston <amyjohnston3sp@gmail.com>
2020-02-18 17:28:55 +0200

    First commit

---
+++ testingMaven1/testingMaven1/testingMaven1/src/main/java/za/co/cput/maven/App.java
@@ -0,0 +1,13 @@
+package za.co.cput.maven;
+
+/**
+ * Hello world!
+ * Author: Amy Johnston (218188773)
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! \n This is my first Maven project");
+    }
+}
